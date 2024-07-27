b84ed1ae0834d823e28909eb1c82666143edd6a7
_phannguyen <116864280+TrongNguyenn1@users.noreply.github.com>
2024-07-27 13:26:16 +0700

    Update Low-Graphics.lua

--- Low-Graphics.lua
+++ Low-Graphics.lua
@@ -6,6 +6,14 @@ for i,v in next, workspace:GetDescendants() do
         v.Transparency = 1
     end)
 end
+for i,v in next, getnilinstances() do
+    pcall(function()
+        v.Transparency = 1
+        for i1,v1 in next, v:GetDescendants() do
+            v1.Transparency = 1
+        end
+    end)
+end
 a = workspace
 a.DescendantAdded:Connect(function(v)
     pcall(function()
